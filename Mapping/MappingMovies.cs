94c4adf849c617f9a5ca2b6b652eee4735e48be0
Oana Ujica <48068668+OanaUjica@users.noreply.github.com>
2021-05-15 08:59:50 +0300

    Automapper and enhanced swagger

---
+++ Mapping/MappingMovies.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Lab1_.NET.Models;
+using Lab1_.NET.ViewModels;
+
+namespace Lab1_.NET.Mapping
+{
+    public class MappingMovies : Profile
+    {
+        public MappingMovies()
+        {
+            CreateMap<Movie, MovieViewModel>();
+
+            CreateMap<MovieViewModel, Movie>();
+        }
+    }
+}
--- ViewModels/MovieViewModel.cs
+++ ViewModels/MovieViewModel.cs
@@ -1,19 +1,26 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace Lab1_.NET.ViewModels
 {
     public class MovieViewModel
     {
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Description is required.")]
         public string Description { get; set; }
 
-        public Genre Genre { get; set; }
 
-        public string DurationInMinutes { get; set; }
+        [EnumDataType(typeof(MovieGenre), ErrorMessage = "Please enter a genre.")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public MovieGenre Genre { get; set; }
+
+        public ushort? DurationInMinutes { get; set; }
 
         public ushort YearOfRelease { get; set; }
 
@@ -21,16 +28,21 @@ namespace Lab1_.NET.ViewModels
 
         public DateTime DateAdded { get; set; }
 
-        public float? Rating { get; set; }
+        [Range(1, 10)]
+        public float Rating { get; set; }
 
         public bool Watched { get; set; }
     }
 
-    public enum Genre
+    public enum MovieGenre
     {
+        [EnumMember(Value = "Action")]
         Action,
+        [EnumMember(Value = "Comedy")]
         Comedy,
+        [EnumMember(Value = "Horror")]
         Horror,
+        [EnumMember(Value = "Thriller")]
         Thriller
     }
 }
